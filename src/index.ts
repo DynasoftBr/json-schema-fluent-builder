b6c378c094aa3e936fc0705821bdda8d46c7889d
Leandro-Albano <leandro.albanoe@hotmail.com>
2017-07-19 20:38:07 -0300

    Removing missing console.log

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,4 @@
 // import { SchemaBuilder } from "./builders/schema-builder";
-import { SchemaBuilderGeneric } from "./builders";
+// import { SchemaBuilderGeneric } from "./builders";
 
-let schema = new SchemaBuilderGeneric({}).definitions("teste", new SchemaBuilderGeneric({}));
-console.log(schema.getSchema());
 export { SchemaBuilder } from "./builders/schema-builder";
